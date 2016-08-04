96c7b3443509aab843799909ee16a148dc7c5092
Aswin Gopalan <AGopalan@incomeresearch.com>
2016-08-04 16:47:36 -0400

    added product component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,16 @@
 import {Component} from 'angular2/core';
+import {ProductListComponent} from './products/product-list.component';
+
 
 @Component({
     selector: 'pm-app',
     template: `
     <div>
         <h1> {{pageTitle}} </h1>
-        <div> My First Component </div>
+        <pm-products>Loading... </pm-products>
     </div>
-    `
+    `,
+    directives: [ProductListComponent]
 })
 
 export class AppComponent{

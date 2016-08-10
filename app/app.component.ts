d4cc2de5d4371384f37c1ac0aacb8e63db0c7dd3
Aswin Gopalan <AGopalan@incomeresearch.com>
2016-08-10 11:26:19 -0400

    Working till Observable subscribe

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component} from 'angular2/core';
+import {HTTP_PROVIDERS} from 'angular2/http';
+import 'rxjs/Rx'; // Load all Features (e.g. Map, Reduce, Filter)
 import {ProductListComponent} from './products/product-list.component';
-
+import {ProductService} from './products/product.service' 
 
 @Component({
     selector: 'pm-app',
@@ -10,7 +12,8 @@ import {ProductListComponent} from './products/product-list.component';
         <pm-products>Loading... </pm-products>
     </div>
     `,
-    directives: [ProductListComponent]
+    directives: [ProductListComponent],
+    providers: [ProductService, HTTP_PROVIDERS]
 })
 
 export class AppComponent{

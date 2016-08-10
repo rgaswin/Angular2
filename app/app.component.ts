e2e8700c9de9119a8eb91d81120bf9bbfc6b71e4
Aswin Gopalan <AGopalan@incomeresearch.com>
2016-08-10 15:33:54 -0400

    Routing Done

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,20 +1,46 @@
 import {Component} from 'angular2/core';
 import {HTTP_PROVIDERS} from 'angular2/http';
+import {ROUTER_PROVIDERS, RouteConfig,ROUTER_DIRECTIVES} from 'angular2/router'
 import 'rxjs/Rx'; // Load all Features (e.g. Map, Reduce, Filter)
 import {ProductListComponent} from './products/product-list.component';
 import {ProductService} from './products/product.service' 
+import {WelcomeComponent} from './home/welcome.component'
+
+
+
 
 @Component({
-    selector: 'pm-app',
+    selector : 'pm-app',
     template: `
     <div>
-        <h1> {{pageTitle}} </h1>
-        <pm-products>Loading... </pm-products>
+      <nav class='navbar navbar-default'>
+        <div class= 'container-fluid'>
+            <a class='navbar-brand'>{{pageTitle}}</a>
+            <ul class='nav navbar-nav'>
+                <li><a [routerLink]= "['Welcome']">Home</a></li>
+                <li><a [routerLink]= "['Products']">Product List</a></li>
+            </ul>   
+        </div>
+      </nav>
+    
+    
+    <div class='container'>
+            <router-outlet>
+            </router-outlet>
     </div>
+</div>
+
     `,
-    directives: [ProductListComponent],
-    providers: [ProductService, HTTP_PROVIDERS]
+    directives: [ROUTER_DIRECTIVES],
+    providers: [ProductService, HTTP_PROVIDERS, ROUTER_PROVIDERS]
 })
+
+@RouteConfig(
+    [
+       {path: '/welcome', name : 'Welcome', component: WelcomeComponent, useAsDefault:true},
+       {path: '/products', name : 'Products', component: ProductListComponent} 
+    ]
+)
 
 export class AppComponent{
         pageTitle : string = 'My Product Management';
